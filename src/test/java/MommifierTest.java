3a3b5a3234bb67d7f7b2ef60c1a38365c173bd15
Rng-nb <18782824515@163.com>
2020-08-03 12:28:22 +0800

    [yafeng Wang] feat: return null when convert given string null

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MommifierTest {
+    @Test
+    void should_return_null_when_convert_given_string_null() {
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert("");
+        assertEquals("", result);
+    }
 }
